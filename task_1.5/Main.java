1fa0c1f0948a1c34ac4796669d9deefde538f434
Aleksandr <82589014+Korsjin@users.noreply.github.com>
2021-04-25 20:10:15 +0300

    Create Main.java

---
+++ task_1.5/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner grad = new Scanner(System.in);
+        double a = grad.nextInt();
+        double r =9.0/5.0;
+        System.out.println(r*a+32);
+    }
+}
